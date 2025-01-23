af316c9a73ca4656c9887c2904aed47cb8bc5bb7
Jed Benhod <jbenhod@gmail.com>
2025-01-23 15:22:11 +0200

    feat: change a bit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![allow(unused)] // for beginning only
+
+use std::net::SocketAddr;
+
+use axum::response::Html;
+use axum::routing::get;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/",
+        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+    );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on {}\n", addr);
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap()
+}
